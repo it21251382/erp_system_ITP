355c8ce8da8d8a989793f980f1f282cbd18b681b
LokoGod <it21079672@my.sliit.lk>
2023-08-24 18:49:03 +0530

    tabbar

--- server/index.js
+++ server/index.js
@@ -5,14 +5,14 @@ import cors from "cors";
 import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
-import orderRoutes from "./routes/order.js"
-import inventoryRoutes from "./routes/inventory.js"
-import supllierRoutes from "./routes/supplier.js"
-import employeeRoutes from "./routes/employee.js"
-import jobRoute from "./routes/job.js"
-import customerRoutes from "./routes/customer.js"
-import generalRoutes from "./routes/general.js"
-import commerceRoutes from "./routes/commerce.js"
+import orderRoutes from "./routes/order.js";
+import inventoryRoutes from "./routes/inventory.js";
+import supllierRoutes from "./routes/supplier.js";
+import employeeRoutes from "./routes/employee.js";
+import jobRoute from "./routes/job.js";
+import customerRoutes from "./routes/customer.js";
+import generalRoutes from "./routes/general.js";
+import commerceRoutes from "./routes/commerce.js";
 
 // Configs
 dotenv.config();
@@ -26,12 +26,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 
 // Routes
-app.use("/order", orderRoutes) // Order management
-app.use("/inventory", inventoryRoutes) //Inventory management
-app.use("/supplier", supllierRoutes) // Supplier managemenr
-app.use("/employee", employeeRoutes) // Employee management
-app.use("/job", jobRoute) // Job management
-app.use("/customer", customerRoutes) // Customer relation
-app.use("/general", generalRoutes) // Dashboard
-app.use("/commerce", commerceRoutes) // ecommerce ??
+app.use("/order", orderRoutes); // Order management
+app.use("/inventory", inventoryRoutes); //Inventory management
+app.use("/supplier", supllierRoutes); // Supplier managemenr
+app.use("/employee", employeeRoutes); // Employee management
+app.use("/job", jobRoute); // Job management
+app.use("/customer", customerRoutes); // Customer relation
+app.use("/general", generalRoutes); // Dashboard
+app.use("/commerce", commerceRoutes); // ecommerce ??
 
+// Mongoose setup
+const PORT = process.env.PORT || 9000;
+mongoose
+  .connect(process.env.Mongo_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server is listening on port ${PORT}...`)
+    );
+  })
+  .catch((error) => console.log(`${error} did not connect`));
