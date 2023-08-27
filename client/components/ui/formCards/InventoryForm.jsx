a169abd68eb682b75e49de5328671cafaff364e6
LokoGod <it21079672@my.sliit.lk>
2023-08-27 12:55:35 +0530

    comit

--- client/components/ui/formCards/InventoryForm.jsx
+++ client/components/ui/formCards/InventoryForm.jsx
@@ -1,37 +1,132 @@
-import React from 'react'
+import React from "react";
 
 const InventoryForm = () => {
   return (
     <div>
-    <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8">
-        <form class="space-y-6" action="#">
-            <h5 class="text-xl font-medium text-gray-900">Sign in to our platform</h5>
-            <div>
-                <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Your email</label>
-                <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@company.com" required />
-            </div>
-            <div>
-                <label for="password" class="block mb-2 text-sm font-medium text-gray-900">Your password</label>
-                <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
-            </div>
-            <div class="flex items-start">
-                <div class="flex items-start">
-                    <div class="flex items-center h-5">
-                        <input id="remember" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300" required />
-                    </div>
-                    <label for="remember" class="ml-2 text-sm font-medium text-gray-900">Remember me</label>
-                </div>
-                <a href="#" class="ml-auto text-sm text-blue-700 hover:underline">Lost Password?</a>
-            </div>
-            <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Login to your account</button>
-            <div class="text-sm font-medium text-gray-500">
-                Not registered? <a href="#" class="text-blue-700 hover:underline">Create account</a>
-            </div>
-        </form>
+      <form class="space-y-4" action="#">
+        <h5 class="text-xl font-medium text-black-500">Product Information</h5>
+        <div>
+          <label
+            for="name"
+            class="block mb-1 text-sm font-medium text-black-500"
+          >
+            Product name *
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+        <div>
+          <label
+            for="sku"
+            class="block mb-1 text-sm font-medium text-black-500"
+          >
+            SKU *
+          </label>
+          <input
+            type="text"
+            name="sku"
+            id="sku"
+            placeholder="SSD-007"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+            required
+          />
+        </div>
+        <div class="flex space-x-4">
+          <select
+            id="countries"
+            class="flex-grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-grey-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>Parent category</option>
+            <option>Canada</option>
+            <option>France</option>
+            <option>Germany</option>
+          </select>
+          <select
+            id="countries"
+            class="flex-grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-white-700 dark:border-white-600 dark:placeholder-grey-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          >
+            <option>Child category</option>
+            <option>Canada</option>
+            <option>France</option>
+            <option>Germany</option>
+          </select>
+        </div>
+        <div>
+          <textarea
+            id="description"
+            rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-white-700 dark:border-white-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Product description..."
+          ></textarea>
+        </div>
+
+        <div className="flex space-x-4">
+          <div className="w-full">
+            <label
+              htmlFor="cost"
+              className="block mb-1 text-sm font-medium text-black-500"
+            >
+              Cost per unit *
+            </label>
+            <input
+              type="text"
+              name="cost"
+              id="cost"
+              placeholder="Rs."
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+
+          <div className="w-full">
+            <label
+              htmlFor="price"
+              className="block mb-1 text-sm font-medium text-black-500"
+            >
+              Selling price per unit *
+            </label>
+            <input
+              type="text"
+              name="price"
+              id="price"
+              placeholder="Rs."
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+
+          <div className="w-full">
+            <label
+              htmlFor="price"
+              className="block mb-1 text-sm font-medium text-black-500"
+            >
+              Warranty *
+            </label>
+            <input
+              type="text"
+              name="price"
+              id="price"
+              placeholder="days"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+              required
+            />
+          </div>
+        </div>
+
+        <button
+          type="submit"
+          class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+        >
+          Save
+        </button>
+      </form>
     </div>
-</div>
+  );
+};
 
-  )
-}
-
-export default InventoryForm
+export default InventoryForm;
--- client/pages/inventory.jsx
+++ client/pages/inventory.jsx
@@ -18,7 +18,7 @@ const Orders = () => {
   return (
     <div className="bg-gray-100 min-h-screen">
       <div className="flex justify-between px-4 pt-4">
-        <h2>Orders</h2>
+        <h2></h2>
         <h2>Welcome Back, Clint</h2>
       </div>
       <TabBar
