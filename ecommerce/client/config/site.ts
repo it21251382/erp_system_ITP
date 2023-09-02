ce5b8898e8dd27ed705e5c923ecc3989bedfc720
LokoGod <it21079672@my.sliit.lk>
2023-09-02 14:26:01 +0530

    new

--- ecommerce/client/config/site.ts
+++ ecommerce/client/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Name",
-  description: "Description",
+  name: "IT Mind",
+  description: "Only for improvement",
   footer: [
     { name: "Home", href: "/" },
     { name: "About", href: "#" },
