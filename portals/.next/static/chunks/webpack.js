679517c13ed60af98d7ad6e6c69517e2bca1889d
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-10-01 20:06:59 +0530

    new

--- portals/.next/static/chunks/webpack.js
+++ portals/.next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f5a3a9e98646ba68"; }
+/******/ 		__webpack_require__.h = function() { return "2fc7a359d9368161"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- portals/app/supplier/signUp/page.tsx
+++ portals/app/supplier/signUp/page.tsx
@@ -1,4 +1,4 @@
-import '../globals.css'
+import '../../globals.css'
 import Link from 'next/link'
 
 export default function Home() {
@@ -12,12 +12,12 @@ export default function Home() {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Log in to your account
+                                Sign-up to your account
                             </h1>
                             <form className="space-y-4 md:space-y-6" action="#">
                                 <div>
-                                    <label htmlFor="phone" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Phone Number</label>
-                                    <input type="text" name="phone" id="phone" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                                    <label htmlFor="sup_name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Company Name</label>
+                                    <input type="text" name="sup_name" id="sup_name" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
@@ -34,14 +34,12 @@ export default function Home() {
                                     </div>
                                     <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
                                 </div>
+                                <br></br>
                                 <Link href="/supplier/dash">
                                     <div className="flex justify-center items-center">
                                         <button className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-20 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">Sign in</button>
                                     </div>
                                 </Link>
-                                <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
-                                </p>
                             </form>
                         </div>
                     </div>
