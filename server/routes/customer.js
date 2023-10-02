61f0b5b7cfe0f5895ba4800bfc14bd79c2462418
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-10-03 03:02:59 +0530

    controller

--- server/routes/customer.js
+++ server/routes/customer.js
@@ -1,8 +1,9 @@
 import express from "express";
 const customerRouter = express.Router();
 
-import { getAllCustomer, createCustomer } from "../controllers/customer.js";
+import { getAllCustomer, createCustomer, getCustomer, updateCustomer, deleteCustomer } from "../controllers/customer.js";
 
 customerRouter.route("/").get(getAllCustomer).post(createCustomer);
+customerRouter.route("/:id").get(getCustomer).patch(updateCustomer).delete(deleteCustomer)
 
 export { customerRouter };
