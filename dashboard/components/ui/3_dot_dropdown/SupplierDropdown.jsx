f9279ec95d4b049c0d86080155451f909596ba26
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-10-02 05:59:35 +0530

    new

--- dashboard/components/ui/3_dot_dropdown/SupplierDropdown.jsx
+++ dashboard/components/ui/3_dot_dropdown/SupplierDropdown.jsx
@@ -76,14 +76,6 @@ const SupplierDropdown = () => {
             </a>
           </li>
         </ul>
-        <div className="py-2">
-          <a
-            href="#"
-            className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-          >
-            Delete Supplier
-          </a>
-        </div>
       </div>
     </div>
   );
