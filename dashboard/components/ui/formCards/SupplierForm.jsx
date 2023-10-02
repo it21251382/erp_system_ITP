9d58d3b07547929988a6bce924f727041cb4b8e8
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-10-02 05:57:13 +0530

    create

--- dashboard/components/ui/formCards/SupplierForm.jsx
+++ dashboard/components/ui/formCards/SupplierForm.jsx
@@ -80,7 +80,6 @@ const SupplierForm = () => {
               type="text"
               name="sup_address"
               id="sup_address"
-              placeholder="SSD-007"
               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
               value={formData.sup_address}
               onChange={handleInputChange}
