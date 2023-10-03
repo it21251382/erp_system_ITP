8ab4dde55188459c8d96852e41717d22fa4acef2
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-10-03 05:44:38 +0530

    latest

--- server/controllers/job.js
+++ server/controllers/job.js
@@ -12,4 +12,35 @@ const createJob = asyncWrapper(async (req, res) => {
   res.status(201).json({ job });
 });
 
-export { getAllJob, createJob };
+const getJob = asyncWrapper(async (req, res, next) => {
+  const { id: jobID } = req.params;
+  const job = await Job.findOne({ _id: jobID });
+  if (!job) {
+    return next(createCustomError(`No job with id: ${jobID}`, 404));
+  }
+  res.status(200).json({ job });
+});
+
+const updateJob = asyncWrapper(async (req, res, next) => {
+  const { id: jobID } = req.params;
+  const job = await Job.findOneAndUpdate({ _id: jobID }, req.body, {
+    new: true,
+    runValidators: true,
+  });
+  if (!job) {
+    return next(createCustomError(`No job with id : ${jobID}`, 404));
+  }
+
+  res.status(200).json({ job });
+});
+
+const deleteJob = asyncWrapper(async (req, res) => {
+  const { id: jobID } = req.params;
+  const job = await Job.findOneAndDelete({ _id: jobID });
+  if (!job) {
+    return next(createCustomError(`No job with Number : ${jobID}`, 404));
+  }
+  res.status(200).json({ job });
+});
+
+export { getAllJob, createJob, getJob, updateJob, deleteJob };
