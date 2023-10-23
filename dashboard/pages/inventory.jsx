8b3b81b6cba50a9fb6eee7eafbc0d3b18d4574ec
Mayukha Palihena <87293241+LokoGod@users.noreply.github.com>
2023-10-24 03:12:32 +0530

    align

--- dashboard/pages/inventory.jsx
+++ dashboard/pages/inventory.jsx
@@ -6,7 +6,7 @@ import TabBar from "../components/TabBar"; // Update the path
 import { FaCubesStacked, FaPenToSquare } from "react-icons/fa6";
 import InventoryForm from "@/components/ui/formCards/InventoryForm.jsx";
 import { fetchInventory } from "./api/invApi";
-import InventoryUpdate from "@/components/ui/updateFormCards/InventoryUpdate"
+import InventoryUpdate from "@/components/ui/updateFormCards/InventoryUpdate";
 
 const Inventory = () => {
   const [selectedTab, setSelectedTab] = useState("Inventory"); // Default selected tab
@@ -46,9 +46,7 @@ const Inventory = () => {
       if (response.ok) {
         // Remove the deleted inventory from the state
         setInventory((prevInventory) =>
-        prevInventory.filter(
-            (inventoryItem) => inventoryItem.sku !== SKU
-          )
+          prevInventory.filter((inventoryItem) => inventoryItem.sku !== SKU)
         );
       } else {
         // Handle error response if needed
@@ -74,59 +72,72 @@ const Inventory = () => {
         {selectedTab === "Inventory" && (
           <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
             <div className="w-full m-auto p-4 border rounded-lg bg-white overflow-y-auto">
-              <div className="my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer">
+              <div className="my-3 p-2 grid grid-cols-4 items-center justify-between cursor-pointer">
                 <span className="font-bold">Name</span>
-                <span className="sm:text-left text-right font-bold">SKU</span>
-                <span className="hidden md:grid font-bold">Stock On Hand</span>
-                <span className="hidden sm:grid font-bold">Reorder level</span>
+                <span className="text-center font-bold">SKU</span>
+                <span className="text-center font-bold">Stock On Hand</span>
+                <span className="text-center font-bold mr-20">Reorder level</span>  
               </div>
+              
               {Array.isArray(inventory) && inventory.length > 0 ? (
-              <ul>
-                {inventory.map((inventoryItem, id) => (
-                  <li
-                    key={id}
-                    className="bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer"
-                  >
-                    <div
-                      className="flex"
-                      onClick={() => {
-                        console.log("Section clicked!");
-                      }}
+                <ul>
+                  {inventory.map((inventoryItem, id) => (
+                    <li
+                      key={id}
+                      className={`bg-gray-50 hover:bg-gray-100 rounded-lg my-3 p-2 grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 items-center justify-between cursor-pointer border ${
+                        inventoryItem.inv_pro_quantity <
+                        inventoryItem.inv_pro_reorder_level
+                          ? "border-2 border-red-500"
+                          : ""
+                      }`}
                     >
-                      <div className="bg-purple-100 p-3 rounded-lg">
-                        <FaCubes className="text-purple-800" />
+                      <div className="flex items-center">
+                        <div className="bg-purple-100 p-3 rounded-lg">
+                          <FaCubes className="text-purple-800" />
+                        </div>
+                        <div className="pl-4">
+                          <p className="text-gray-800 font-bold">
+                            {inventoryItem.inv_pro_name}
+                          </p>
+                          <p className="text-gray-800 text-sm">
+                            Rs.{inventoryItem.inv_pro_selling.toLocaleString()}
+                          </p>
+                        </div>
                       </div>
-                      <div className="pl-4">
-                        <p className="text-gray-800 font-bold">
-                          {inventoryItem.inv_pro_name}
-                        </p>
-                        <p className="text-gray-800 text-sm">
-                          Rs.{inventoryItem.inv_pro_selling.toLocaleString()}
-                        </p>
+
+                      <div className="text-center">
+                        <span className={"bg-green-200 p-2 rounded-lg"}>
+                          {inventoryItem.sku}
+                        </span>
                       </div>
-                    </div>
-                    <p className="text-gray-600 sm:text-left text-right">
-                      <span className={"bg-green-200 p-2 rounded-lg"}>
-                        {inventoryItem.sku}
-                      </span>
-                    </p>
-                    <p className="hidden md:flex">{inventoryItem.inv_pro_quantity}</p>
-                    <div className="sm:flex hidden justify-between items-center">
-                      <p>{inventoryItem.inv_pro_reorder_level}</p>
-                      <div className="ml-3">
-                        <button
-                          className="px-4 py-2 text-white bg-red-600 hover:bg-red-800 rounded-lg transition duration-300 ease-in-out"
-                          onClick={() =>
-                            handleDeleteInventory(inventoryItem.sku)
+                      <div className="text-center">
+                        <span
+                          className={
+                            inventoryItem.inv_pro_quantity <
+                            inventoryItem.inv_pro_reorder_level
+                              ? "text-red-500 font-bold"
+                              : ""
                           }
                         >
-                          Delete
-                        </button>
+                          {inventoryItem.inv_pro_quantity}
+                        </span>
                       </div>
-                    </div>
-                  </li>
-                ))}
-              </ul>
+                      <div className="text-center sm:flex justify-between items-center">
+                        <p className="ml-20">{inventoryItem.inv_pro_reorder_level}</p>
+                        <div className="ml-3">
+                          <button
+                            className="px-4 py-2 text-white bg-red-600 hover:bg-red-800 rounded-lg transition duration-300 ease-in-out"
+                            onClick={() =>
+                              handleDeleteInventory(inventoryItem.sku)
+                            }
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  ))}
+                </ul>
               ) : (
                 <p>No Inventory data to show</p>
               )}
